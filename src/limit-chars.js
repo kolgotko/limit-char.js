9d4a095f35d25d159f5a9617f43f96469adfe71b
Igor Bykov <con29rus@icloud.com>
2016-10-26 13:08:47 +0300

    first commit

---
+++ src/limit-chars.js
@@ -0,0 +1,62 @@
+class LimitChars {
+
+	constructor(element = null) {
+
+		this._element = element;
+
+		if (element.hasAttribute('maxlength'))
+			this.setMaxLength(element.getAttribute('maxlength'));
+
+		else this._maxLength = 0;
+
+		if (element.hasAttribute('data-count-output'))
+			this._countOutput = element.getAttribute('data-count-output');
+
+		else this._countOutput = null;
+
+		element.addEventListener('input', () => { this._input(); });
+
+		this._input();
+
+	}
+
+	_input() {
+
+		let value = this._element.value;
+		let maxLength = parseInt(this._maxLength);
+
+		if (maxLength && value.length > maxLength)
+			this._element.value = value.substr(0, maxLength);
+
+		if (this._countOutput) {
+
+			let nodes = document.querySelectorAll(this._countOutput);
+			let elems = [].slice.call(nodes);
+
+			elems.forEach((elem) => {
+
+				if (maxLength) { elem.innerHTML = maxLength - value.length; }
+
+				else elem.innerHTML = value.length;
+
+			});
+
+		}
+
+	}
+
+	setCountOutput(selector) {
+
+		this._countOutput = selector;
+		return this;
+
+	}
+
+	setMaxLength(length) {
+
+		this._maxLength = parseInt(length);
+		return this;
+
+	}
+
+}
